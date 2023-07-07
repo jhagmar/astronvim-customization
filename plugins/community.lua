48c7f169375cab18b38751dc0e28a09faed74624
Jonas Hagmar <jonas.hagmar@gmail.com>
2023-07-07 20:11:42 +0200

    Enable relevant language packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,4 +7,11 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.go"},
+  { import = "astrocommunity.pack.lua"},
+  { import = "astrocommunity.pack.docker"},
+  { import = "astrocommunity.pack.json"},
+  { import = "astrocommunity.pack.markdown"},
+  { import = "astrocommunity.pack.python"},
+  { import = "astrocommunity.pack.toml"},
+  { import = "astrocommunity.pack.yaml"},
 }
